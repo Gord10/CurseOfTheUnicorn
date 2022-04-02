6d6ea92b75bd126c40309b47f190214d5aea4b77
Gord10 <akk13us@yahoo.com>
2022-04-02 15:03:12 +0300

    First player sprite and background

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,11 +7,13 @@ public class Player : MonoBehaviour
     public float baseSpeed = 3f;
 
     private Rigidbody2D rigidbody;
+    private SpriteRenderer spriteRenderer;
     private Vector2 desiredMovementDirection = new Vector2();
 
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Start is called before the first frame update
@@ -23,8 +25,19 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Read the player input
         desiredMovementDirection.x = Input.GetAxis("Horizontal");
         desiredMovementDirection.y = Input.GetAxis("Vertical");
+
+        //Flip the sprite renderer according to the X direction that the player wants to go
+        if(desiredMovementDirection.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if(desiredMovementDirection.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
 
         //We don't want to player to move too fast when they are moving diagonally
         desiredMovementDirection = Vector2.ClampMagnitude(desiredMovementDirection, 1f);
@@ -32,6 +45,7 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
+        //Move the player along the desired direction
         rigidbody.velocity = desiredMovementDirection * baseSpeed;
     }
 }
